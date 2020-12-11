52a0b2b952b8e10ae31d345cd770f28c29dbe5e2
gsanchez13 <gisellesanchez@pursuit.org>
2020-12-10 23:55:53 -0500

    creates jumbtron

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,7 +1,7 @@
 const Landing = () => {
     return (
-        <div>
-            Welcome to Book Exchange
+        <div className= "container">
+            <h3 className="jumbotron text-center">Welcome to Book Exchange</h3>
         </div>
     )
 };
